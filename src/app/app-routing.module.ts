204a8309068e4bdf1145cfefd534f9b6e272ecad
Benjamin SIMON <bensim.602@gmail.com>
2021-12-12 18:40:01 +0100

    Setup project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppHomeComponent} from "./components/app-home/app-home.component";
+
+const routes: Routes = [
+  { path: '', component: AppHomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppHomeComponent } from './components/app-home/app-home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
